e811428c0485afbb183308c7f71467a694253067
ReRandom <R.E.P@yandex.ru>
2017-08-22 17:40:15 +0700

    add IPv4 parser and new system highlight

--- sniff.c
+++ sniff.c
@@ -6,47 +6,54 @@
 #include <stdint.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <pcap/pcap.h>
 
-void callback_function(u_char *arg, const struct pcap_pkthdr *pkthdr,
-		const u_char *packet)
+struct color
+{
+	struct color *next;
+	bpf_u_int32 start;
+	bpf_u_int32 end;
+	char *str;
+
+};
+
+struct color* find_color(struct color* colors, bpf_u_int32 num)
+{
+	struct color *ret;
+	for(ret = colors; ret != NULL; ret = ret->next)
+		if(num >= ret->start && num <= ret->end)
+			return ret;
+	return NULL;
+}
+
+void print_raw(u_char *arg, const struct pcap_pkthdr *pkthdr,
+		const u_char *packet, struct color *colors)
 {
 	bpf_u_int32 i; //like pkthdr->len
-	printf("destination:\x1b[34m");
-	for(i = 0; i < 6; ++i)
-		printf(" %02X", (int)packet[i]);
-	printf("\x1b[0m source:\x1b[33m");
-	for(; i < 12; ++i)
-		printf(" %02X", (int)packet[i]);
-	printf("\x1b[0m type: \x1b[31m");
-	for(; i < 14; ++i)
-		printf(" %02X", (int)packet[i]);
-	printf("\x1b[0m\n");
 	for(i = 0; i < pkthdr->len; i+=*(size_t*)arg)
 	{
 		bpf_u_int32 j;
-		if(i < 6)
-			printf("\x1b[34m");
-		else if(i < 12)
-			printf("\x1b[33m");
-		else if(i < 14)
-			printf("\x1b[31m");
+		struct color *current = NULL;
 		for(j = 0; j < *(size_t*)arg; ++j)
 		{
-			if(i+j == 6)
-				printf("\x1b[33m");
-			if(i+j == 12)
-				printf("\x1b[31m");
-			if(i+j == 14)
-				printf("\x1b[0m");
+			if(current == NULL)
+			{
+				current = find_color(colors, i+j);
+				if(current != NULL)
+					printf("%s", current->str);
+			}
 			if(i+j < pkthdr->len)
 				printf("%02X ", (int)packet[i+j]);
 			else
 				printf("   ");
+			if(current != NULL && i+j+1 >= current->end)
+			{
+				printf("\x1b[0m");
+				current = NULL;
+			}
 		}
-		if(i+j < 14)
-			printf("\x1b[0m");
-		printf("| ");
+		printf("\x1b[0m| ");
 		for(j = 0; j < *(size_t*)arg; ++j)
 		{
 			if(i+j >= pkthdr->len)
@@ -59,6 +66,95 @@ void callback_function(u_char *arg, const struct pcap_pkthdr *pkthdr,
 		printf("\n");
 	}
 	printf("\n");
+}
+
+void print_ip(u_char *arg, const struct pcap_pkthdr *pkthdr,
+		const u_char *packet, struct color *colors)
+{
+	struct color *temp = (struct color*)malloc(sizeof(struct color));
+	colors->next = temp;
+
+	printf("(IPv4)\ntotal length: %hu (0x%04hx)",
+			ntohs(*(unsigned short*)(&packet[2])),
+			ntohs(*(unsigned short*)(&packet[2])));
+	printf(" TTL: %hhu Protocol: %hhu (0x%02hhx)\n", packet[8],
+			packet[9], packet[9]);
+
+	temp->str = "\x1b[35m";
+	temp->start = 12;
+	temp->end = 15;
+	printf("IP source: \x1b[35m%hhu.%hhu.%hhu.%hhu\x1b[0m (\x1b[35m0x%08x\x1b[0m)\n",
+			packet[12], packet[13], packet[14], packet[15],
+			ntohl(*(unsigned*)(&packet[12])));
+
+	temp->next = (struct color*)malloc(sizeof(struct color));
+	temp = temp->next;
+	temp->str = "\x1b[36m";
+	temp->start = 16;
+	temp->end = 19;
+	temp->next = NULL;
+	printf("IP destination: \x1b[36m%hhu.%hhu.%hhu.%hhu\x1b[0m (\x1b[36m0x%08x\x1b[0m)\n",
+			packet[16], packet[17], packet[18], packet[19],
+			ntohl(*(unsigned*)(&packet[16])));
+}
+
+void free_color(struct color *color)
+{
+	if(color->next != NULL)
+		free_color(color->next);
+	free(color);
+}
+
+void print_packet(u_char *arg, const struct pcap_pkthdr *pkthdr,
+		const u_char *packet)
+{
+	bpf_u_int32 i; //like pkthdr->len
+	struct color colors;
+	printf("destination:\x1b[34m");
+	for(i = 0; i < 6; ++i)
+		printf(" %02X", (int)packet[i]);
+	colors.start = 0;
+	colors.end = 5;
+	colors.str = "\x1b[34m";
+
+	struct color *temp = (struct color*)malloc(sizeof(struct color));
+	printf("\x1b[0m source:\x1b[33m");
+
+	colors.next = temp;
+	temp->str = "\x1b[33m";
+	temp->start = i;
+
+	for(; i < 12; ++i)
+		printf(" %02X", (int)packet[i]);
+	temp->end = i-1;
+
+	temp = (struct color*)malloc(sizeof(struct color));
+	colors.next->next = temp;
+	temp->next = NULL;
+	temp->str = "\x1b[31m";
+	temp->start = i;
+	printf("\x1b[0m type: \x1b[31m");
+	for(; i < 14; ++i)
+		printf(" %02X", (int)packet[i]);
+	printf(" \x1b[0m");
+	temp->end = i-1;
+	//if type == 0x0800
+	if(packet[12] == 0x8 && packet[13] == 0x0)
+	{
+		print_ip(arg, pkthdr, &packet[i], temp);
+		while(temp->next != NULL)
+		{
+			temp->next->start += i;
+			temp->next->end += i;
+			temp = temp->next;
+		}
+	}
+	else if(packet[12] == 0x8 && packet[13] == 0x6)
+		printf("(ARP)\n");
+	else
+		printf("\n");
+	print_raw(arg, pkthdr, packet, &colors);
+	free_color(colors.next);
 	fflush(stdout);
 }
 
@@ -116,7 +212,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "error in pcap_open: %s\n", str_err);
 		return 1;
 	}
-	int ret = pcap_loop(dev, size, callback_function, (u_char*)print_bytes);
+	int ret = pcap_loop(dev, size, print_packet, (u_char*)print_bytes);
 	pcap_close(dev);
 	return 0;
 }
